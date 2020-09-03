ce62ce766c479733c93c53c2cbf8f06ec1182c52
BiancaZYCao <caozy@shanghaitech.edu.cn>
2020-09-03 23:38:08 +0800

    Make ghost moving in 3 ways

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,8 +22,8 @@ class Ghost {
       this.speed = speed;
       this.currentIndex = startIndex;
       this.previousIndex = startIndex;
-      this.isScared = false
-      this.timerId = NaN
+      this.isScared = false;
+      this.timerId = 1000000;
   }
 }
 
@@ -83,6 +83,7 @@ export class AppComponent implements OnInit{
     new Ghost('inky', 351, 320),
     new Ghost('clyde', 379, 500)
   ]
+  timeInterval;
 
   @ViewChild('grid') grid: ElementRef;
   @ViewChild('score') score: ElementRef;
@@ -175,9 +176,155 @@ export class AppComponent implements OnInit{
 
   }
 
-  moveGhost(ghost) {
-    // please implement this.
+  moveGhost(ghost:Ghost) {
+    //console.log("reach ghost move" + ghost.className);
+    //set time interval based on speed
+    ghost.timerId = setInterval( () => {
+        //remove ghost from curr location
+      this.squares[ghost.currentIndex].classList.remove('ghost');
+      this.squares[ghost.currentIndex].classList.remove(ghost.className);
+      //first stage : helping ghost moving out of lair
+      if(ghost.className != "clyde" && this.squares[ghost.currentIndex].classList.contains('ghost-lair')){
+        this.moveGhostOut(ghost);
+      }
+      //second stage : helping ghost moving in the grid    
+      else if (ghost.className === "blinky")
+      {//strategy1 try moving up first if failing, try moving right then left then down
+        this.moveGhostUpRightLeftDown(ghost);
+      }
+      else if (ghost.className === "pinky")
+      {//strategy2 try moving randomly
+        this.moveGhostRandom(ghost);
+      }
+      else if (ghost.className === "inky")
+      {//strategy3 chasing pac-man
+        this.moveGhostChase(ghost);
+      }
+      //add ghost to new location
+      this.squares[ghost.currentIndex].classList.add('ghost');
+      this.squares[ghost.currentIndex].classList.add(ghost.className);
+    },ghost.speed*3);
+
   }
+
+  moveGhostOut(ghost:Ghost) {
+    if(ghost.currentIndex%this.width<=12){//move right
+      ghost.currentIndex += 1;}
+    else if(ghost.currentIndex%this.width>=15){//move left
+      ghost.currentIndex -= 1;}
+    else {//move upper
+      ghost.currentIndex -= this.width;
+    }
+  }
+
+  moveGhostUpRightLeftDown(ghost:Ghost) {
+    if(!this.squares[ghost.currentIndex - this.width].classList.contains('wall')
+              && (ghost.currentIndex - this.width != ghost.previousIndex)
+      )
+        {
+          ghost.previousIndex = ghost.currentIndex;
+          ghost.currentIndex -= this.width;
+          //console.log("ghost Blinky able to move up" + ghost.currentIndex);
+        }
+      else if (!this.squares[ghost.currentIndex + 1].classList.contains('wall')
+      && (ghost.currentIndex +1 != ghost.previousIndex)
+      ){
+        ghost.previousIndex = ghost.currentIndex;
+        ghost.currentIndex += 1;
+      }
+      else if (!this.squares[ghost.currentIndex - 1].classList.contains('wall')
+      && (ghost.currentIndex - 1 != ghost.previousIndex)
+      ){
+        ghost.previousIndex = ghost.currentIndex;
+        ghost.currentIndex -= 1;
+      }
+      else if (!this.squares[ghost.currentIndex + this.width].classList.contains('wall')
+      && (ghost.currentIndex + this.width != ghost.previousIndex)
+      ){
+        ghost.previousIndex = ghost.currentIndex;
+        ghost.currentIndex += this.width;
+      }
+
+  }
+
+  moveGhostRandom(ghost:Ghost) {
+    const directions = [-1, +1, -this.width, +this.width];
+    let direction = directions[Math.floor(Math.random() * directions.length)];
+    console.log(direction);
+    if (
+      !this.squares[ghost.currentIndex + direction].classList.contains('wall') &&
+      !this.squares[ghost.currentIndex + direction].classList.contains('ghost-lair')
+    ) {//add direction to current Index
+    ghost.previousIndex = ghost.currentIndex;
+    ghost.currentIndex += direction;
+  }
+}
+moveGhostChase(ghost:Ghost) {
+  let directionsOrder = [];
+  console.log("ghost: inky chase");
+  //let direction = directions[Math.floor(Math.random() * directions.length)];
+  if ((ghost.currentIndex % this.width) <= (this.pacmanCurrentIndex % this.width)//to right
+    && (ghost.currentIndex/this.width) <= (this.pacmanCurrentIndex/this.width)//down
+    ){
+      directionsOrder = [+1,+this.width,-1,-this.width];     
+  }
+  else if (
+    (ghost.currentIndex % this.width) >= (this.pacmanCurrentIndex % this.width)//left
+  && (ghost.currentIndex/this.width) <= (this.pacmanCurrentIndex/this.width)//down
+  ){
+    directionsOrder = [-1,+this.width,+1,-this.width];     
+  }
+  else if (
+  (ghost.currentIndex % this.width) >= (this.pacmanCurrentIndex % this.width)//left
+  && (ghost.currentIndex/this.width) >= (this.pacmanCurrentIndex/this.width)//up
+  ){directionsOrder = [-1,-this.width,+1,+this.width];     
+  }
+  else {directionsOrder = [+1,+this.width,-1,-this.width];} 
+  //directionsOrder.forEach(direction => {
+  for ( let i=0;i<4;i++){
+    let direction = directionsOrder[i];
+    if (ghost.currentIndex + direction != ghost.previousIndex &&
+      !this.squares[ghost.currentIndex + direction].classList.contains('wall') &&
+        !this.squares[ghost.currentIndex + direction].classList.contains('ghost-lair')
+      ){
+      ghost.previousIndex = ghost.currentIndex;
+      ghost.currentIndex += direction;
+      console.log("ghost: inky chase moving direction  "+ direction);
+      break;
+      }
+  }
+}
+moveGhostChase2(ghost:Ghost) {
+  let direction = 0;
+  console.log("ghost: inky chase");
+  let test = 0;
+  //let direction = directions[Math.floor(Math.random() * directions.length)];
+  while (test < 5){
+    if (direction != 1 && (ghost.currentIndex % this.width) < (this.pacmanCurrentIndex % this.width)//to right
+    ){
+      console.log(direction);
+      direction = 1;}
+    if (direction != -1 && (ghost.currentIndex % this.width) >= (this.pacmanCurrentIndex % this.width)//left
+    ){console.log(direction);
+      direction = -1;}
+    if (direction != -this.width && (ghost.currentIndex/this.width) >= (this.pacmanCurrentIndex/this.width)//up
+    ){console.log(direction);
+      direction = -this.width;}
+    if (direction != +this.width && (ghost.currentIndex/this.width) < (this.pacmanCurrentIndex/this.width)//up
+    ){console.log(direction);
+      direction = +this.width;}
+    if (!this.squares[ghost.currentIndex + direction].classList.contains('wall') &&
+        !this.squares[ghost.currentIndex + direction].classList.contains('ghost-lair')
+      ){
+        ghost.previousIndex = ghost.currentIndex;
+        ghost.currentIndex += direction;
+        console.log("ghost: inky chase moving direction  "+ direction);
+        break;
+      }
+    test += 1;
+}
+}
+
 
   checkForGameOver(squaresX, pacmanCurrentIndexX) {
       if (squaresX[pacmanCurrentIndexX].classList.contains('ghost') &&
